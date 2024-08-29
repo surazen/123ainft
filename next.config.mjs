aed7f8122080ae1046f0060a44e76014c55db72d
Sura <105344847+surazen@users.noreply.github.com>
2024-08-29 09:30:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   webpack: function (config, options) {
-    const { isServer } = options;
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
     };
     return config;
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://123ainft.vercel.app/" }, // replace with your domain
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.blockfrost.io/:path*",
+      },
+    ];
+  },
   env: {
     STABILITY_API_KEY: process.env.STABILITY_API_KEY,
-    PINATA_JWT_TOKEN: process.env.PINATA_JWT_TOKEN,
   },
 };
 
